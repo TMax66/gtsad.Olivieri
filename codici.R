b1f403fd7499620c25bbb2062b8e70e5142a034d
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2023-02-27 11:16:07 +0100

    update

--- codici.R
+++ codici.R
@@ -1,17 +1,39 @@
 library(tidyverse)
 library(openxlsx)
 library(readxl)
+library(here)
 
-dati <- read_excel("data/dati.xlsx")
-
-
-dt <- dt %>% 
-  filter( `Prova - Singola` = !str_detect(`Prova - Singola`, "Identificazione"))
+dati <- read_excel(here("data", "dati.xlsx"))
 
 
-x <- dati %>% 
-  mutate(new_esito = str_remove(`Esito precodificato`,  '.*(?=con)'), 
-         new_esito = str_remove(new_esito,'con ' ))
+dt <- dati %>%
+  filter(is.na(esito)) %>%
+  distinct() %>% 
+  
+  group_by(specie, stadio) %>% 
+  mutate(stadio = casefold(stadio), 
+         stadio = ifelse(stadio %in% c("femmina", "maschio"), "adulto", stadio )) %>% 
+  
+  count() 
+  
 
+
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+#    
+
+# x <- dati %>% 
+#   mutate(new_esito = str_remove(`Esito precodificato`,  '.*(?=con)'), 
+#          new_esito = str_remove(new_esito,'con ' ))
+# 
 
  
